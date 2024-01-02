700595ad0577bc1a94671aa4ad6794e532f1e4d3
JS <zks145@naver.com>
2024-01-02 17:54:55 +0900

    feat : 주문 DTO 작성

---
+++ src/main/java/khan/mobile/dto/ProductOrderDto.java
@@ -0,0 +1,17 @@
+package khan.mobile.dto;
+
+import khan.mobile.entity.Product_order;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class ProductOrderDto {
+
+    private int quantity;
+    private String text;
+    private Long product_id;
+    private Long store_id;
+    private Long user_id;
+
+}
