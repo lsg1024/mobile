4c561b68b5bd9f1c9603f13ac189ace5ef7c4582
JS <zks145@naver.com>
2024-01-04 19:10:57 +0900

    test : 상세 주문 테스트 코드 작성

--- src/test/java/khan/mobile/service/ProductOrderServiceTest.java
+++ src/test/java/khan/mobile/service/ProductOrderServiceTest.java
@@ -9,6 +9,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -71,26 +72,28 @@ class ProductOrderServiceTest {
 
         assertNotNull(newStore);
         assertThat(newStore.getStore_id()).isEqualTo(7L);
-        assertThat(newStore.getStore_name()).isEqualTo("테스트 스토어")
+        assertThat(newStore.getStore_name()).isEqualTo("테스트 스토어");
     }
 
     @Test
     void ProductOrder() {
         //given
-        Users user = Users.builder().user_id(0L).build();
-        Products product = Products.builder().product_id(0L).build();
-        Stores store = Stores.builder().store_id(0L).build();
+        Users newUser = Users.builder().id("테스트를 위한 아이디").password("테스트를 위한 비밀번호").role(Role.USER).build();
 
-        em.persist(user);
-        em.persist(product);
-        em.persist(store);
+        Products newProduct = Products.builder().product_name("테스트 상품").product_color("금색").product_weight(10.4f).product_size(15f).product_other("테스트 내용").product_image("img/").build();
+
+        Stores newStore = Stores.builder().store_name("테스트 스토어").build();
+
+        em.persist(newUser);
+        em.persist(newProduct);
+        em.persist(newStore);
 
         ProductOrderDto productOrderDto = ProductOrderDto.builder()
                 .quantity(5)
                 .text("테스트 코드")
-                .product_id(product.getProduct_id())
-                .store_id(store.getStore_id())
-                .user_id(user.getUser_id())
+                .product_id(newProduct.getProduct_id())
+                .store_id(newStore.getStore_id())
+                .user_id(newUser.getUser_id())
                 .build();
         //when
         Product_order createOrder = productOrderService.createOrder(productOrderDto);
