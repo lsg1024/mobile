07aeb3279e93f344e1d381c9dd2c8ed3a76054ce
JS <zks145@naver.com>
2024-01-07 12:20:44 +0900

    test : 공장 생성 테슽트 작성

--- src/test/java/khan/mobile/entity/FactoriesTest.java
+++ src/test/java/khan/mobile/entity/FactoriesTest.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
@@ -21,6 +24,20 @@ class FactoriesTest {
 
     @Test
     void createFactory() {
+        Factories factories = Factories.builder()
+                .factory_name("테스트 공장")
+                .build();
+
+        factoryRepository.save(factories);
+
+        em.flush();
+        em.clear();
+
+        Optional<Factories> findFactory = factoryRepository.findById(factories.getFactory_id());
+
+        assertTrue(findFactory.isPresent(), "공장 아이디를 찾을 수 없음 : " + factories.getFactory_id());
+
+        assertEquals(factories.getFactory_id(), findFactory.get().getFactory_id(), "공장 아이디가 존재합니다.");
 
     }
 }
