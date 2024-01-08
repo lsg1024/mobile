aded2af81fc0c8e0176005743e3853d311ddd274
JS <zks145@naver.com>
2024-01-08 22:25:51 +0900

    feat : StoreExcelDto -> StoreDto 이름 변경

---
+++ src/main/java/khan/mobile/dto/StoreDto.java
@@ -0,0 +1,23 @@
+package khan.mobile.dto;
+
+import khan.mobile.entity.Stores;
+import lombok.*;
+
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreDto {
+
+    private Long store_id;
+    private String store_name;
+
+    @Builder
+    public static StoreDto storeName(Stores stores) {
+        return StoreDto.builder()
+                .store_id(stores.getStore_id())
+                .store_name(stores.getStore_name())
+                .build();
+    }
+
+}
