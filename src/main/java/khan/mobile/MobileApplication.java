21ba0ec644e44903100ec827009244fe3dd964af
lsg1024 <zks145@naver.com>
2024-01-10 19:13:45 +0900

    feat : Page Been 등록

--- src/main/java/khan/mobile/MobileApplication.java
+++ src/main/java/khan/mobile/MobileApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -20,6 +21,14 @@ public class MobileApplication {
 	@Bean
 	public AuditorAware<String> auditorProvider() {
 		return () -> Optional.of(UUID.randomUUID().toString());
+	}
+
+	@Bean
+	public PageableHandlerMethodArgumentResolverCustomizer customizer() {
+		return p -> {
+			p.setOneIndexedParameters(true);
+			p.setMaxPageSize(10);
+		};
 	}
 
 }
