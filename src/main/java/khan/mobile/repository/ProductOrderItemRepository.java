2279ef89e8d74a01ac0590f8c7cb612e81cd495c
JS <zks145@naver.com>
2024-01-13 08:22:18 +0900

    feat : findByProductOrderIdIn Item 리스트 호출 쿼리

--- src/main/java/khan/mobile/repository/ProductOrderItemRepository.java
+++ src/main/java/khan/mobile/repository/ProductOrderItemRepository.java
@@ -2,6 +2,16 @@ package khan.mobile.repository;
 
 import khan.mobile.entity.Product_orderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductOrderItemRepository extends JpaRepository<Product_orderItem, Long> {
+
+    @Query("SELECT poi FROM Product_orderItem poi WHERE poi.product_order.product_order_id IN :orderIds")
+    List<Product_orderItem> findByProductOrderIdIn(@Param("orderIds") List<Long> orderIds);
 }
+
+
+
