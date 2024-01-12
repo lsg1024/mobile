ff349bf8eac3670c3641fa2e319778af2d7aea5d
lsg1024 <zks145@naver.com>
2024-01-12 17:15:06 +0900

    feat : ProductOrderItemRepository 추가

---
+++ src/main/java/khan/mobile/repository/ProductOrderItemRepository.java
@@ -0,0 +1,7 @@
+package khan.mobile.repository;
+
+import khan.mobile.entity.Product_orderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductOrderItemRepository extends JpaRepository<Product_orderItem, Long> {
+}
