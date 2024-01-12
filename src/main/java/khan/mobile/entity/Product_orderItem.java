1ac2fc58b09611c9d619203fa2212cdb979d03d3
lsg1024 <zks145@naver.com>
2024-01-12 17:11:45 +0900

    feat : product_orderItem_quantity 추가 제품 수량 식별

--- src/main/java/khan/mobile/entity/Product_orderItem.java
+++ src/main/java/khan/mobile/entity/Product_orderItem.java
@@ -2,6 +2,7 @@ package khan.mobile.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,15 +11,14 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Product_orderItem {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_orderItem_id")
     private Long product_orderItem_id;
 
-    private String product_orderItem_name;
     private String product_orderItem_color;
     private Float product_orderItem_size;
-    private Float product_orderItem_weight;
     private String product_orderItem_other;
+    private int product_orderItem_quantity;
 
     @ManyToOne
     @JoinColumn(name = "product_order_id")
@@ -28,4 +28,14 @@ public class Product_orderItem {
     @JoinColumn(name = "product_id")
     private Products products;
 
+    @Builder
+    public Product_orderItem(Long product_orderItem_id, String product_orderItem_color, Float product_orderItem_size, String product_orderItem_other, int product_orderItem_quantity, Product_order product_order, Products products) {
+        this.product_orderItem_id = product_orderItem_id;
+        this.product_orderItem_color = product_orderItem_color;
+        this.product_orderItem_size = product_orderItem_size;
+        this.product_orderItem_other = product_orderItem_other;
+        this.product_orderItem_quantity = product_orderItem_quantity;
+        this.product_order = product_order;
+        this.products = products;
+    }
 }
