7ca36b12b8780fbe06f071dfd71c42e4474807e0
JS <zks145@naver.com>
2024-01-13 08:29:01 +0900

    feat :  특정 사용자 ID에 해당하는 모든유저 아이디 목록 생성 쿼리

--- src/main/java/khan/mobile/repository/ProductOrderRepository.java
+++ src/main/java/khan/mobile/repository/ProductOrderRepository.java
@@ -2,7 +2,13 @@ package khan.mobile.repository;
 
 import khan.mobile.entity.Product_order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductOrderRepository extends JpaRepository<Product_order, Long> {
 
+    @Query("select po from Product_order po where po.user.user_id = :user_id")
+    List<Product_order> findByUserId(@Param("user_id") Long user_id);
 }
