343a76aa9d8d39f7672095d158e810d2530d0e09
JS <zks145@naver.com>
2024-01-16 21:14:02 +0900

    feat : UserService 코드 추가

---
+++ src/main/java/khan/mobile/service/UserService.java
@@ -0,0 +1,13 @@
+package khan.mobile.service;
+
+import khan.mobile.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
