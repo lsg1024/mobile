09988982efe391f2ebf5e1752b4b2e46e5bd5354
JS <zks145@naver.com>
2024-01-17 07:45:26 +0900

    feat : Kakao Login 미완

--- src/main/java/khan/mobile/controller/UsersController.java
+++ src/main/java/khan/mobile/controller/UsersController.java
@@ -1,10 +1,4 @@
 package khan.mobile.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@RequiredArgsConstructor
 public class UsersController {
-
 }
