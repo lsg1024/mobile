368dd82303bd4ad0f79e9f82127e29d72ac55c0e
JS <zks145@naver.com>
2024-01-19 15:53:27 +0900

    feat : 카카오 소셜 로그인으로 회원 가입

--- src/main/java/khan/mobile/repository/UserRepository.java
+++ src/main/java/khan/mobile/repository/UserRepository.java
@@ -4,4 +4,5 @@ import khan.mobile.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByEmail(String email);
 }
--- src/main/java/khan/mobile/service/UserService.java
+++ src/main/java/khan/mobile/service/UserService.java
@@ -1,5 +1,6 @@
 package khan.mobile.service;
 
+import khan.mobile.entity.Role;
 import khan.mobile.entity.Users;
 import khan.mobile.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +19,25 @@ public class UserService {
     public Long createUser(String email) {
         Users user = Users.builder()
                 .email(email)
+                .role(Role.USER)
                 .build();
 
         userRepository.save(user);
         log.info("새로운 회원 저장 완료");
         return user.getUser_id();
+    }
+
+    @Transactional
+    public String double_checkUserinfo(String email) {
+        log.info("인증 이메일 = {}" ,email);
+        Users existingUser = userRepository.findByEmail(email);
+
+        if (existingUser == null) {
+            // 새로운 사용자 등록 페이지로 이동
+            return "htmlpages/~~";
+
+        } else {
+            return "이미 가입된 사용자 입니다";
+        }
     }
 }
