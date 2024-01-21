57e26fcd2dbe8290b2e545783500ad64d373304d
JS <zks145@naver.com>
2024-01-21 22:51:33 +0900

    feat : 요청 실패 응답 처리 dto 생성

--- src/main/java/khan/mobile/dto/ResponseDto.java
+++ src/main/java/khan/mobile/dto/ResponseDto.java
@@ -3,10 +3,15 @@ package khan.mobile.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter @Setter
 public class ResponseDto {
     private String response;
+    private Map<String, String> errors;
     public ResponseDto(String response) {
         this.response = response;
     }
+    public ResponseDto(Map<String, String> errors) {
+        this.errors = errors;}
 }
