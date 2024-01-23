050575dac1eeed208a7dfb13ceafd67779204613
lsg1024 <zks145@naver.com>
2024-01-23 15:23:45 +0900

    feat : 데이터 반환 방식 타입 변경

--- src/main/java/khan/mobile/kakao/KakaoController.java
+++ src/main/java/khan/mobile/kakao/KakaoController.java
@@ -16,10 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 @Slf4j
 public class KakaoController {
+
     private final KakaoTokenJsonData kakaoTokenJsonData;
     private final KakaoUserInfo kakaoUserInfo;
-
-    private final UserService userService;
 
     @GetMapping("/index")
     public String index() {
@@ -28,13 +27,13 @@ public class KakaoController {
 
     @GetMapping("/oauth")
     @ResponseBody
-    public Long kakaoOauth(@RequestParam("code") String code) {
+    public String kakaoOauth(@RequestParam("code") String code) {
         log.info("인가 코드를 이용하여 토큰을 받습니다.");
         KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
         log.info("토큰에 대한 정보입니다.{}",kakaoTokenResponse);
         KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token());
         log.info("회원 정보 입니다.{}",userInfo);
 
-        return userService.createUser(userInfo.getKakao_account().getEmail());
+        return userInfo.getKakao_account().getEmail();
     }
 }
