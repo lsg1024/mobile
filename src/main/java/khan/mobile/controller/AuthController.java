1aad72322ff1916d33856e9ac57c1bd25865ad8c
lsg1024 <zks145@naver.com>
2024-01-23 15:21:28 +0900

    feat : jwt 토근

---
+++ src/main/java/khan/mobile/controller/AuthController.java
@@ -0,0 +1,30 @@
+package khan.mobile.controller;
+
+import khan.mobile.entity.Users;
+import khan.mobile.jwt.JwtUtil;
+import khan.mobile.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody Users user) {
+        Optional<Users> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser != null) {
+            String token = JwtUtil.generateToken(user.getUser_id(), String.valueOf(user.getRole()));
+            return ResponseEntity.ok(token);
+        } else {
+            return ResponseEntity.status(401).body("Unauthorized");
+        }
+    }
+}
