17ba50fdf08a00bb7dfe63e79254b79ec845807f
lsg1024 <zks145@naver.com>
2024-01-26 13:03:13 +0900

    feat : SpringSecurity Post 접근 제한

---
+++ src/main/java/khan/mobile/configuration/EncoderConfig.java
@@ -0,0 +1,16 @@
+package khan.mobile.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncoderConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
