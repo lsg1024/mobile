6f747c008ccaff1065caede696c72e93170540a6
lsg1024 <zks145@naver.com>
2024-02-03 01:33:57 +0900

    feat : 상품 검색 위한 JPA 메소드 생성 엔티티의 _ 가 포함될 경우 JPA에서 인식 불가능

--- src/main/java/khan/mobile/repository/ProductRepository.java
+++ src/main/java/khan/mobile/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package khan.mobile.repository;
 import khan.mobile.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Products, Long> {
+    List<Products> findByProductNameContaining(String name);
 }
