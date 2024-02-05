2f7114da7ab58c18ba86b00ef319fcf23d7845ae
JS <zks145@naver.com>
2024-02-05 18:43:15 +0900

    feat : 상점 수정 컨트롤러 추가

--- src/main/java/khan/mobile/service/StoreService.java
+++ src/main/java/khan/mobile/service/StoreService.java
@@ -1,20 +1,14 @@
 package khan.mobile.service;
 
-import com.querydsl.core.QueryFactory;
-import com.querydsl.core.QueryResults;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import khan.mobile.dto.StoreDto;
-import khan.mobile.entity.QStores;
 import khan.mobile.entity.Stores;
 import khan.mobile.repository.StoreRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -47,17 +41,15 @@ public class StoreService {
     }
 
     // 상점 수정
-    public String updateStores(Long store_id, String storeName) {
+    public void updateStores(String store_id, StoreDto storeDto) {
 
-        Optional<Stores> store = storeRepository.findById(store_id);
+        Optional<Stores> store = storeRepository.findById(Long.parseLong(store_id));
 
         Stores stores = Stores.builder()
-                .store_id(store.get().getStore_id())
-                .store_name(storeName)
+                .store_id(store.orElseThrow().getStore_id())
+                .store_name(storeDto.getStore_name())
                 .build();
 
         storeRepository.save(stores);
-
-        return "update";
     }
 }
