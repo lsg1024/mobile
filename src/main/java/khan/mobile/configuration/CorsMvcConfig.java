a6da1182f2022c5dc91ead6ae133ef76d57d398a
lsg1024 <zks145@naver.com>
2024-02-21 13:38:07 +0900

    feat : 클라이언트 Cors 허용

--- src/main/java/khan/mobile/configuration/CorsMvcConfig.java
+++ src/main/java/khan/mobile/configuration/CorsMvcConfig.java
@@ -5,11 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000"); // React 앱 URL
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:3000");
     }
 }
